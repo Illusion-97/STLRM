aacde6a2372ce25a757d3414b0cee6b014fd00c7
Yanis Adekalom <yanis971.60@gmail.com>
2023-04-25 08:55:57 +0200

    Initial commit

---
+++ src/main/java/dwn/slrm/SlrmApplication.java
@@ -0,0 +1,13 @@
+package dwn.slrm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlrmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SlrmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dwn/slrm/SlrmApplicationTests.java
@@ -0,0 +1,13 @@
+package dwn.slrm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlrmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
