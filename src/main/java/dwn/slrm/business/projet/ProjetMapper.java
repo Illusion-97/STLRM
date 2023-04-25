831732fafd1ee911939563d0c68fc1751770aed8
Yanis Adekalom <yanis971.60@gmail.com>
2023-04-25 11:32:44 +0200

    Mappers

---
+++ src/main/java/dwn/slrm/business/projet/ProjetMapper.java
@@ -0,0 +1,8 @@
+package dwn.slrm.business.projet;
+
+import dwn.slrm.generic.mappers.GenericMapper;
+import org.mapstruct.Mapper;
+
+@Mapper
+public interface ProjetMapper extends GenericMapper<Projet,ProjetDto> {
+}
