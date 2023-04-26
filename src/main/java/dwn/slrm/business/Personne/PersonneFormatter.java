f0bacf419be092199e907919a2329130a9169fe8
Yanis Adekalom <yanis971.60@gmail.com>
2023-04-26 17:26:50 +0200

    AbstractFormatter

---
+++ src/main/java/dwn/slrm/business/Personne/PersonneFormatter.java
@@ -0,0 +1,8 @@
+package dwn.slrm.business.Personne;
+
+import dwn.slrm.generic.formatters.AbstractFormatter;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PersonneFormatter implements AbstractFormatter<PersonneDto> {
+}
---
+++ src/main/java/dwn/slrm/business/competence/CompetenceFormatter.java
@@ -0,0 +1,8 @@
+package dwn.slrm.business.competence;
+
+import dwn.slrm.generic.formatters.AbstractFormatter;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CompetenceFormatter implements AbstractFormatter<CompetenceDto> {
+}
