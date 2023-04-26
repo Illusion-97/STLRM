86c2fcf847f10059f8b4ff04a688ddad1d78adc7
Yanis Adekalom <yanis971.60@gmail.com>
2023-04-26 16:23:17 +0200

    HOTFIX : byId.html

---
+++ src/main/java/dwn/slrm/business/DossierProjet/DossierProjetController.java
@@ -0,0 +1,17 @@
+package dwn.slrm.business.DossierProjet;
+
+import dwn.slrm.business.competence.Competence;
+import dwn.slrm.business.competence.CompetenceDto;
+import dwn.slrm.business.competence.CompetenceService;
+import dwn.slrm.generic.Constants;
+import dwn.slrm.generic.controllers.AbstractController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(Constants.COMPETENCE_PREFIX)
+public class DossierProjetController extends AbstractController<Competence, CompetenceDto, CompetenceService> {
+    protected DossierProjetController(CompetenceService service) {
+        super(service, Constants.COMPETENCE_PREFIX);
+    }
+}
