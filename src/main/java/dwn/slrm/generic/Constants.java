74c9ed0773579a778a9db2284dad3122558a4922
Yanis Adekalom <yanis971.60@gmail.com>
2023-04-26 14:33:20 +0200

    CompetenceController

--- src/main/java/dwn/slrm/generic/Constants.java
+++ src/main/java/dwn/slrm/generic/Constants.java
@@ -2,4 +2,5 @@ package dwn.slrm.generic;
 
 public abstract class Constants {
     public static final String RESUME_PREFIX = "resumes";
+    public static final String COMPETENCE_PREFIX = "competence";
 }
